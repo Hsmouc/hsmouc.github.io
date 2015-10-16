d899631a1da89a022446315a543da465e4f2d0c3
hsmouc <heshiming001@icloud.com>
2015-10-16 23:23:16 +0800

    ?

--- js/backtotop.js
+++ js/backtotop.js
@@ -1,32 +1,14 @@
-window.onload = function(){
-    var obtn = document.getElementById('#back-top');
-    //获取页面可视区的高度
-    var timer = null;
-    var isTop = true;
-    window.onscroll = function(){
-        var osTop = document.body.scrollTop;
-        var clientHeight = document.documentElement.clientHeight;
-        if(osTop >= clientHeight){
-            obtn.style.display="block";
-        } else {
-            obtn.style.display="none";
-        }
-        if(!isTop){
-            clearInterval(timer);
-        }
-        isTop = false;
+$("#back-top").hide();
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#back-top').fadeIn();
+    } else {
+      $('#back-top').fadeOut();
     }
-    obtn.onclick = function(){
-        //设置定时器
-        timer = setInterval(function(){
-                            var osTop = document.body.scrollTop;
-                            var ispeed = Math.floor(-osTop / 5);
-                            document.documentElement.scrollTop = document.body.scrollTop = osTop + ispeed;
-                            isTop = true;
-                            if (osTop == 0){
-                            clearInterval(timer);
-                            }
-                            },30);
-    }
-}
-
+  });
+ 
+ $("#scrollTop").click(function() {  
+        $("html,body").animate({scrollTop:0}, 800);  
+        return false;  
+    });
